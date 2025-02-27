6afda3758aed8e232fc4306e0617688412bd200a
Gabriel Ribeiro Rodrigues <103051424+GabrielRibeiroRodrigues@users.noreply.github.com>
2025-02-27 07:59:14 -0300

    1069

---
+++ maratonas/beecrowd/1069.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+
+#define d(x) cout<<#x<<" = "<<x<<endl;
+
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define f first
+#define s second
+#define r(i,n) for(int i = 0; i < (n); i++)
+typedef long long ll;
+typedef pair<int, int> ii;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main()
+{ 
+string palavra;
+int n = 0,contadesq = 0,contaddir = 0,resp=0;
+cin >> n;
+for(int i = 0; i < n; i++){
+    contadesq = 0;contaddir = 0;resp=0;
+    cin >> palavra;
+    for(int j = 0; j < palavra.size(); j++){
+        if(palavra[j] == '<'){
+            contadesq++;
+        }
+        if(palavra[j] == '>'){
+            if(contadesq > 0){
+                resp++;
+                contadesq--;
+            }
+        }
+    }
+    cout << resp << endl;
+}
+
+
+}
